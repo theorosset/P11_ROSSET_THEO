1fb103e8ba54675c2dd0182110047de7b3cb4255
Rosset <theo.rosset@outlook.fr>
2023-04-11 13:28:53 +0200

    refacto(models): add article models

---
+++ src/app/models/articles.ts
@@ -0,0 +1,13 @@
+
+export type Article = {
+    id: string
+    title: string
+    cover: string
+    pictures: string[]
+    description: string
+    host: object
+    rating: number
+    location: number
+    equipements: string[]
+    tags: string[]
+}
